1c11ae5ebd927e804321607f2bc7d9c2ea54f94e
Hamza-Azeem1 <hamzaazeem023@gmail.com>
2024-06-12 15:40:27 +0500

    updated

--- backend/index.js
+++ backend/index.js
@@ -11,8 +11,11 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-// Enable CORS for all routes
-app.use(cors());
+app.use(cors({
+    origin: ["https://seo-news.vercel.app"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}));
 
 // Serve static files from the "uploads" directory
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
